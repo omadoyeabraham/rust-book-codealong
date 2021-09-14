9feb5349014d8851f968d6de71c049e171f1b1fa
Omadoye Abraham <abraham.o@turing.com>
2021-09-14 10:49:52 +0100

    Added comments to guessing game from chapter 2

--- chapter-2/guessing_game/src/main.rs
+++ chapter-2/guessing_game/src/main.rs
@@ -2,18 +2,25 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+// Create a simple guessing game that prompts a user for number
+// and informs the user if their choice is higher than, lower than or equal to
+// a randomly generated secret number
+
 fn main() {
+    // Generate the secret number
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("Guess the number!");
 
     loop {
         println!("Please input your guess.");
 
+        // Get the user's input from the command line
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read the line");
 
+        // Ensure that the user's input is a valid number
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -22,6 +29,8 @@ fn main() {
             }
         };
         println!("You guessed: {}", guess);
+
+        // Inform the user if their guess is <, > or = to the secret number
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
